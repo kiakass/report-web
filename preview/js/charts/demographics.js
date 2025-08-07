fb4bcfa0184635238f95070324de88a823063acc
junhk <junhk@example.com>
2025-08-07 14:15:03 +0900

    � 구조 리팩토링: preview → dev/frontend/, prod/frontend/ 생성

--- preview/js/charts/demographics.js
+++
@@ -1,5 +0,0 @@
-// demographics.js - 인구통계 차트
-
-export function initDemographicsCharts() {
-  // TODO: Chart.js로 차트 초기화
-} 
--- preview/js/charts/trends.js
+++
@@ -1,5 +0,0 @@
-// trends.js - 트렌드 차트
-
-export function initTrendsChart() {
-  // TODO: Chart.js로 차트 초기화
-} 
